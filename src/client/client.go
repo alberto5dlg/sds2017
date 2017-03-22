6977f120500705cb04da4e66c98bf7ca0b3fbb94
Fernando Marín Sanz <fernandomarinsanz@gmail.com>
2017-03-22 13:13:12 +0100

    Login: user y password JSON

--- src/client/client.go
+++ src/client/client.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/http"
+	"os"
 )
 
 var urlServer = "https://127.0.0.1:8081"
 
+type userRes struct {
+	User     string
+	Password string
+}
+
+func chkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func menu() int {
-	var opcion int
+	var opcion = 0
 	for opcion <= 0 || opcion >= 4 {
 		fmt.Printf("Aplicación SDS Seguridad\n")
 		fmt.Printf("---------------------------------------\n")
@@ -18,10 +33,7 @@ func menu() int {
 		fmt.Printf("2 - Registro\n")
 		fmt.Printf("3 - Salir\n")
 		fmt.Printf("Opción: ")
-		n, err := fmt.Scanf("%d\n", &opcion)
-		if err != nil {
-			fmt.Println(n, err)
-		}
+		fmt.Scanf("%d\n", &opcion)
 	}
 	return opcion
 }
@@ -31,30 +43,68 @@ func ignorarHTTPS() http.Client {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	client := &http.Client{Transport: tr}
-
 	return *client
 }
 
 func peticionGET() {
 	client := ignorarHTTPS()
 	resp, err := client.Get(urlServer)
-	if err != nil {
-		panic(err)
-	}
+	chkError(err)
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+	chkError(err)
 	fmt.Println(string(body))
 
 }
 
-func main() {
+func login() bool {
 
-	opc := menu()
-	switch opc {
-	case 1:
-		peticionGET()
-		break
-	case 3:
-		println("Adios")
+	var correct = false
+	fmt.Printf("\n__Login__\n")
+
+	var user string
+	fmt.Printf("User: ")
+	err := fmt.Scanf("%s\n", &user)
+	chkError(err)
+
+	var password string
+	fmt.Printf("Password: ")
+	err = fmt.Scanf("%s\n", &password)
+	chkError(err)
+
+	//llamada al servidor / comprobar paswword
+	m := userRes{user, password}
+	mJSON, err := json.Marshal(m)
+	chkError(err)
+	os.Stdout.Write(mJSON)
+	fmt.Printf("\n")
+
+	if correct == false {
+		fmt.Printf("User or password Error\n\n")
+	} else {
+		fmt.Printf("Welcome!\n\n")
+	}
+	return correct
+}
+
+func main() {
+	var url = urlServer + ":" + portServer
+	// connect to this socket
+	conn, _ := net.Dial(typeConexion, url)
+	var opcion = menu()
+	if opcion == 1 {
+		login()
+	}
+	for {
+
+		// read in input from stdin
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+		// send to socket
+		fmt.Fprintf(conn, text+"\n")
+		// listen for reply
+		//message, _ := bufio.NewReader(conn).ReadString('\n')
+		//fmt.Print("Message from server: " + message)
 	}
 }
